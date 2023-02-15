2d6e1f372c58e41664a9fddca9f6a004d25b133c
lhmag89 <119267098+lhmag89@users.noreply.github.com>
2023-02-14 21:56:14 -0800

    minor fix

--- EmployeeSQL/employees_analysis.sql
+++ EmployeeSQL/employees_analysis.sql
@@ -20,7 +20,7 @@ FROM employees
 INNER JOIN dept_manager
   ON employees.emp_no = dept_manager.emp_no
 INNER JOIN departments
-  ON dept_manager.dept_no = departments.dept_no;
+  ON dept_manager.dept_no = departments.dept_no
 ;
 
 -- 4. List of employees with dept number, employee number, last name, first name and department name
